1fd78579f5206f6fb07ef8f1bbc1d3c9e5314c1c
mikhailkaryamin <karyamin@uchi.ru>
2024-10-19 11:39:01 +0300

    fix system test

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -1,6 +1,8 @@
 require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @post = posts(:one)
   end
@@ -11,32 +13,38 @@ class PostsTest < ApplicationSystemTestCase
   end
 
   test "should create post" do
+    sign_in users(:one)
+
     visit posts_url
     click_on "New post"
 
-    fill_in "Body", with: @post.body
-    fill_in "Category", with: @post.category_id
-    fill_in "Title", with: @post.title
-    click_on "Create Post"
+    fill_in "Текст поста", with: @post.body
+    select("Category1", from: "Категория")
+    fill_in "form_header_test_id", with: @post.title
+    click_on "Опубликовать пост"
 
     assert_text "Post was successfully created"
     click_on "Back"
   end
 
   test "should update Post" do
+    sign_in users(:one)
+
     visit post_url(@post)
     click_on "Edit this post", match: :first
 
-    fill_in "Body", with: @post.body
-    fill_in "Category", with: @post.category_id
-    fill_in "Title", with: @post.title
-    click_on "Update Post"
+    fill_in "Текст поста", with: @post.body
+    select("Category1", from: "Категория")
+    fill_in "form_header_test_id", with: @post.title
+    click_on "Опубликовать пост"
 
     assert_text "Post was successfully updated"
     click_on "Back"
   end
 
   test "should destroy Post" do
+    sign_in users(:one)
+
     visit post_url(@post)
     click_on "Destroy this post", match: :first
 
